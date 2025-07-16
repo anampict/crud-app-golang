d4f2254f54cf15eb54f1d5127ed54eaa64ed145c
anampict <anampict@gmail.com>
2025-07-16 20:57:39 +0700

    inisiasi models dan controller

---
+++ backend/controllers/product_controller.go
@@ -0,0 +1,127 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"crud-golang/backend/models"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var productCollection *mongo.Collection
+
+func initMongo() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+
+	}
+	ctxhttp,cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctxhttp)
+
+	if err != nil {
+		panic(err)
+	}
+
+	productCollection = client.Database("crud_golang").Collection("products")
+}
+
+//get all products
+
+func GetProducts (c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	cursor, err := productCollection.Find(ctx, bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching products"})
+		return
+	}
+	var products []models.Product
+
+	for cursor.Next(ctx) {
+		if err := cursor.Err(); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		} 
+	}
+
+	c.JSON(http.StatusOK, products)
+}
+
+//menambahkan product
+
+func CreateProduct(c *gin.Context) {
+    var product models.Product
+    if err := c.ShouldBindJSON(&product); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    res, err := productCollection.InsertOne(ctx, product)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, res)
+}
+
+
+// PUT update product
+func UpdateProduct(c *gin.Context) {
+    idParam := c.Param("id")
+    id, err := primitive.ObjectIDFromHex(idParam)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+        return
+    }
+
+    var product models.Product
+    if err := c.ShouldBindJSON(&product); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    update := bson.M{"$set": product}
+    _, err = productCollection.UpdateOne(ctx, bson.M{"_id": id}, update)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": "Updated successfully"})
+}
+
+
+// DELETE product
+func DeleteProduct(c *gin.Context) {
+    idParam := c.Param("id")
+    id, err := primitive.ObjectIDFromHex(idParam)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+        return
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    _, err = productCollection.DeleteOne(ctx, bson.M{"_id": id})
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": "Deleted successfully"})
+}
+
